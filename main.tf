e43c7779c3bb4ebea99f4e4bea291094164d71bc
Hilton Giesenow <hiltatwork@giesenow.com>
2019-11-26 16:07:55 +0200

    adding version

--- main.tf
+++ main.tf
@@ -1,19 +1,20 @@
 resource "null_resource" "module_depends_on" {
-  triggers = {
-    value = "${length(var.module_depends_on)}"
-  }
+	triggers = {
+		value = "${length(var.module_depends_on)}"
+	}
 }
 
 resource "null_resource" "bot_channel_msteams" {
-  triggers = {
-    resource_group_name = var.resource_group_name
-    bot_name 			= var.bot_name
-    # enabled  			= var.enabled
-  }
+	triggers = {
+		resource_group_name = var.resource_group_name
+		bot_name 			= var.bot_name
+		# enabled  			= var.enabled
+	}
 
-  provisioner "local-exec" {
-    command = "az bot msteams create --resource-group ${var.resource_group_name} --name ${var.bot_name}"
-  }
+	provisioner "local-exec" {
+		command = "az bot msteams create --resource-group ${var.resource_group_name} --name ${var.bot_name}"
+	}
 
-	depends_on                       = ["null_resource.module_depends_on"]
+	depends_on = ["null_resource.module_depends_on"]
+	version    = v0.1.0
 }
